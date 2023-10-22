a5b5b4ef066291e91bfdfe9c399c76371722f72f
“damir_gritsinenko” <gritsinenko.damir@bk.ru>
2023-10-23 02:53:48 +0300

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Banner } from "../Banner/Banner";
+import "./App.scss";
+
+export const App = () => {
+  return (
+    <div className="app">
+      <Banner />
+    </div>
+  );
+};
